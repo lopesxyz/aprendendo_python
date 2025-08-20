8fe4313527d9dd1c468a9e61e5b65fd8a7fb4dd8
Lucas <lukksgamer57@gmail.com>
2025-08-20 13:00:31 -0300

    aprendi sobre prints

---
+++ python.py
@@ -0,0 +1,16 @@
+# Definindo variáveis
+
+nome = "lucas"
+idade = 17
+altura = 1.70
+situacaoTrabalho = False
+
+# Exibindo os dados com a função print
+
+print("hello world") 
+print('a soma é:',2+3)
+print('a subtração é:' + nome)
+print('a multiplicação {}'. format(5*5))
+print(f'a divisão é {10/2}')
+
+print (F' seu nome é {nome}, sua idade é {idade}, sua altura é {altura} e sua situação de trabalho é {situacaoTrabalho}')
