9c95c69327a99e4e3f002dbf6cbb445cc993de27
lopesxyz <lukksgamer57@gmail.com>
2025-08-27 12:53:44 -0300

    atividades

---
+++ atividadesAula2.py
@@ -0,0 +1,27 @@
+#atividadesAula2
+
+#1
+nome = input('Digite seu nome ')
+print ('Boas-Vindas',nome)
+
+#2
+n = int(input('Digite um número'))
+dobro = n*2
+triplo = n*3
+raiz = n**0.5
+
+print (' O DOBRO É',dobro)
+print (' O TRIPLO É',triplo)
+print (' A RAIZ QUADRADA É',raiz)
+
+#3
+m = int(input('Digite um número '))
+
+cm = m*100
+mm = m*1000
+
+print ('Centímetros: ',cm)
+print ('Milímetros: ',mm)
+
+#4
+
